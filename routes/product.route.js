8d1058f16326594a717b3a4fcf873be8c919e004
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-06-27 22:28:23 +0530

    day2

---
+++ routes/product.route.js
@@ -0,0 +1,72 @@
+const { Router } = require('express');
+const { Product } = require('../models/products.model');
+
+
+const productRoute = Router();
+
+productRoute.get("/", async (req, res) => {
+    try {
+        let data = await Product.find();
+        res.status(200).send({
+            isError: false,
+            data
+        })
+    } catch (error) {
+        res.status(400).send({
+            isError: true,
+            error
+        })
+    }
+})
+
+productRoute.post('/', async (req, res) => {
+    try {
+        let { title, image1, image2, gender, category, price, description, size, discount, quantity } = req.body;
+        let prod = new Product({ title, image1, image2, gender, category, price, description, size, discount, quantity });
+        await prod.save();
+        res.status(200).send({
+            isError: false,
+            message: "product created successfully",
+            product: prod
+        })
+    } catch (error) {
+        res.status(400).send({
+            isError: true,
+            error: error.message
+        })
+    }
+})
+
+productRoute.patch('/:id', async (req, res) => {
+    let { id } = req.params;
+    try {
+        await Product.findByIdAndUpdate(id, req.body);
+        res.status(200).send({
+            isError: false,
+            message: "product updated successfully"
+        })
+    } catch (error) {
+        res.status(400).send({
+            isError: true,
+            error
+        })
+    }
+})
+
+productRoute.delete('/:id', async (req, res) => {
+    let { id } = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).send({
+            isError: false,
+            message: "product deleted successfully"
+        })
+    } catch (error) {
+        res.status(400).send({
+            isError: true,
+            error
+        })
+    }
+})
+
+module.exports = { productRoute };
--- server.js
+++ server.js
@@ -5,12 +5,14 @@ require('dotenv').config();
 const {connection} = require('./db/mongodb');
 const { redisConnect } = require('./db/redis');
 const {userRoute} = require('./routes/user.route');
+const {productRoute} = require('./routes/product.route');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/user',userRoute);
+app.use('/product',productRoute);
 
 app.listen(process.env.port,async()=>{
     try {
