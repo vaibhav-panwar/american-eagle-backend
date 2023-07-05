87451b16d3a9223b2a59bb7287b476bb5fa54e46
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-07-05 17:42:51 +0530

    address route completed

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const {connection} = require('./db/mongodb');
+const { connection } = require('./db/mongodb');
 const { redisConnect } = require('./db/redis');
-const {userRoute} = require('./routes/user.route');
-const {productRoute} = require('./routes/product.route');
+const { userRoute } = require('./routes/user.route');
+const { productRoute } = require('./routes/product.route');
 const { cartRoute } = require('./routes/cart.route');
 const { orderRouter } = require('./routes/order.route');
 const { addressRoute } = require('./routes/address.route');
@@ -15,14 +15,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/user',userRoute);
-app.use('/product',productRoute);
-app.use('/cart',cartRoute);
-app.use('/order',orderRouter);
-app.use('/address',addressRoute);
+app.use('/user', userRoute);
+app.use('/product', productRoute);
+app.use('/cart', cartRoute);
+app.use('/order', orderRouter);
+app.use('/address', addressRoute);
 
 
-app.listen(process.env.port,async()=>{
+app.listen(process.env.port, async () => {
     try {
         await connection
         await redisConnect
