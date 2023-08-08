8c28dd56057f268473a901596a0255808462955a
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-08-09 00:05:40 +0530

    filtering

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,13 +1,35 @@
-const { Router } = require('express');
+const { Router, query } = require('express');
 const { Product } = require('../models/products.model');
-const {productAdminAuth} = require('../middlewares/productAuth');
+const { productAdminAuth } = require('../middlewares/productAuth');
 
 const productRoute = Router();
 
 productRoute.get("/", async (req, res) => {
     try {
-        const totalCount = await Product.countDocuments();
-        let data = await Product.find();
+        const page = parseInt(req.query.page) || 1; // default to page 1
+        const limit = parseInt(req.query.limit) || 9; // default to limit of 10 products per page
+        const skipIndex = (page - 1) * limit;
+        let sortField = {};
+        let searchQuery = {};
+        if (req.query.sort) {
+            if (req.query.sort == "asc") {
+                sortField.price = 1;
+            }
+            else {
+                sortField.price = -1;
+            }
+        }
+        if (req.query.gender) {
+            searchQuery.gender = req.query.gender
+        }
+        if (req.query.category) {
+            searchQuery.category = req.query.category
+        }
+        if (req.query.price) {
+           searchQuery.price = {$lte:req.query.price};
+        }
+        const totalCount = await Product.find(searchQuery).countDocuments();
+        let data = await Product.find(searchQuery).skip(skipIndex).limit(limit).sort(sortField);
         res.set({
             'X-Total-Count': totalCount,
             'Access-Control-Expose-Headers': 'X-Total-Count'
@@ -24,7 +46,7 @@ productRoute.get("/", async (req, res) => {
     }
 })
 
-productRoute.post('/',productAdminAuth, async (req, res) => {
+productRoute.post('/', productAdminAuth, async (req, res) => {
     try {
         let { title, image1, image2, gender, category, price, description, size, discount, quantity } = req.body;
         let prod = new Product({ title, image1, image2, gender, category, price, description, size, discount, quantity });
@@ -42,7 +64,7 @@ productRoute.post('/',productAdminAuth, async (req, res) => {
     }
 })
 
-productRoute.patch('/:id',productAdminAuth, async (req, res) => {
+productRoute.patch('/:id', productAdminAuth, async (req, res) => {
     let { id } = req.params;
     try {
         await Product.findByIdAndUpdate(id, req.body);
@@ -58,7 +80,7 @@ productRoute.patch('/:id',productAdminAuth, async (req, res) => {
     }
 })
 
-productRoute.delete('/:id',productAdminAuth, async (req, res) => {
+productRoute.delete('/:id', productAdminAuth, async (req, res) => {
     let { id } = req.params;
     try {
         await Product.findByIdAndDelete(id);
